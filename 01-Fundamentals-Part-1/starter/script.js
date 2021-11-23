6ca614606fb1396270e24315abd8ac4bf7453d62
Rob Whelan <hallwayraptor@gmail.com>
2021-11-23 15:43:59 -0400

    finish up to strings and template literals

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+const country = 'Canada'
+const continent = 'North America'
+let canadaPop = 36
+const isIsland = false
+const language = 'English'
+let finlandPop = 6
+let averagePop = 33
+
+let description = `${country} is in ${continent} and its ${canadaPop} million people speak ${language}`
+
+console.log(description)
+
+if (canadaPop > finlandPop) {
+    console.log(`Canada has a higher population than Finland`)
+};
+
+if (canadaPop > averagePop) {
+    console.log(`Canada has a higher population than average`)
+} else {
+    console.log(`error`)
+}
+
+
+
+
+
+
+console.log(country)
+console.log(continent)
+console.log(canadaPop)
+console.log(isIsland)

61e01943065635997969166205a8d04f912d5f79
Rob Whelan <hallwayraptor@gmail.com>
2021-11-23 18:30:30 -0400

    finish Logical Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,31 +1,31 @@
 const country = 'Canada'
 const continent = 'North America'
 let canadaPop = 36
+let population = canadaPop
 const isIsland = false
 const language = 'English'
 let finlandPop = 6
 let averagePop = 33
+// let description = `${country} is in ${continent} and its ${canadaPop} million people speak ${language}`
+// if (canadaPop > finlandPop) {
+//     console.log(`Canada has a higher population than Finland`)
+// };
 
-let description = `${country} is in ${continent} and its ${canadaPop} million people speak ${language}`
+// if (canadaPop > averagePop) {
+//     console.log(`Canada has a higher population than average`)
+// } else {
+//     console.log(`error`)
+// }
+// console.log(description)
+// console.log(country)
+// console.log(continent)
+// console.log(canadaPop)
+// console.log(isIsland)
 
-console.log(description)
+//find out if my country is good for Sarah
 
-if (canadaPop > finlandPop) {
-    console.log(`Canada has a higher population than Finland`)
-};
-
-if (canadaPop > averagePop) {
-    console.log(`Canada has a higher population than average`)
+if(language === 'English' && population < 50 && !isIsland) {
+    console.log('You should live in Canada!')
 } else {
-    console.log(`error`)
+    console.log('Canada is not a good fit.')
 }
-
-
-
-
-
-
-console.log(country)
-console.log(continent)
-console.log(canadaPop)
-console.log(isIsland)

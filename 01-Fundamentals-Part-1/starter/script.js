0d393fdd092833b4d59cc56a829c809ef42ce94d
Rob Whelan <hallwayraptor@gmail.com>
2021-11-23 20:47:38 -0400

    finish fundamentals 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,16 +1,37 @@
-const country = 'Canada'
-const continent = 'North America'
-let canadaPop = 36
-let population = canadaPop
-const isIsland = false
-const language = 'English'
-let finlandPop = 6
-let averagePop = 33
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const country = "Canada";
+// const continent = "North America";
+// let canadaPop = 36;
+// let population = canadaPop;
+// const isIsland = false;
+// const language = "english";
+// let finlandPop = 6;
+// let averagePop = 33;
+// function popCheck () {
+//   console.log(population > 33 ? 'Canada\'s population is above average' : 'Canada\'s population is below average')
+// }
+// popCheck();
 // let description = `${country} is in ${continent} and its ${canadaPop} million people speak ${language}`
 // if (canadaPop > finlandPop) {
 //     console.log(`Canada has a higher population than Finland`)
 // };
-
 // if (canadaPop > averagePop) {
 //     console.log(`Canada has a higher population than average`)
 // } else {
@@ -21,11 +42,29 @@ let averagePop = 33
 // console.log(continent)
 // console.log(canadaPop)
 // console.log(isIsland)
-
 //find out if my country is good for Sarah
-
-if(language === 'English' && population < 50 && !isIsland) {
-    console.log('You should live in Canada!')
-} else {
-    console.log('Canada is not a good fit.')
-}
+// if (language === "English" && population < 50 && !isIsland) {
+//   console.log("You should live in Canada!");
+// } else {
+//   console.log("Canada is not a good fit.");
+// }
+// switch (language) {
+//   case "chinese":
+//     console.log("MOST number of native speakers");
+//     break;
+//   case "spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "english":
+//     console.log("3rd place");
+//     break;
+//   case "hindi":
+//     console.log("4th place");
+//     break;
+//   case "arabic":
+//     console.log("5th place");
+//     break;
+//   default:
+//     console.log("Great language too");
+//     break;
+// }

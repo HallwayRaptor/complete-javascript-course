5487aa30210122e02fda2302728e464c6fa14b71
Rob Whelan <hallwayraptor@gmail.com>
2021-11-23 23:40:26 -0400

    start coding challenge 3

---
+++ coding-challenges/03-coding-challenge/script.js
@@ -0,0 +1,32 @@
+// script.js
+// 1. Calculate the average score for each team
+// 2. Compare the team's average score to determine the winner of the completition.
+// 3. bonus: include a requirement for a minimum score of 100.
+// 4. bonus2: minimum score also applies to a draw!
+
+// data //
+// Dolphins score 96, 108, 89
+// Koalas score 88, 91, 110
+
+// data bonus //
+// Dolphins 97, 112, 101
+// Koalas 109, 95, 123
+
+// data bonus2 //
+// Dolphins 97, 112, 101
+// Koalas 109, 95, 106
+
+//Dolphins scores
+let dGame1 = 96;
+let dGame2 = 108;
+let dGame3 = 89;
+//Koalas scores
+let kGame1 = 88;
+let kGame2 = 91;
+let kGame3 = 110;
+//number of games
+let numGames = 3;
+//avg of Dolphins games
+let dolphinAvg = (dGame1 + dGame2 + dGame3) / numGames;
+//avg of Koala's games
+let koalaAvg = (kGame1 + kGame2 + kGame3) / numGames;

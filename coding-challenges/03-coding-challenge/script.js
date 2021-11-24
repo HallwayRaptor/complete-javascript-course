c71d4e16136b6f26465e4aba66cc78cea1bcbad6
Rob Whelan <hallwayraptor@gmail.com>
2021-11-24 11:06:03 -0400

    finish challenge 3

--- coding-challenges/03-coding-challenge/script.js
+++ coding-challenges/03-coding-challenge/script.js
@@ -17,16 +17,36 @@
 // Koalas 109, 95, 106
 
 //Dolphins scores
-let dGame1 = 96;
-let dGame2 = 108;
-let dGame3 = 89;
+let dGame1 = 97;
+let dGame2 = 112;
+let dGame3 = 101;
 //Koalas scores
-let kGame1 = 88;
-let kGame2 = 91;
-let kGame3 = 110;
+let kGame1 = 109;
+let kGame2 = 95;
+let kGame3 = 123;
 //number of games
 let numGames = 3;
 //avg of Dolphins games
 let dolphinAvg = (dGame1 + dGame2 + dGame3) / numGames;
+dolphinAvg = Math.round(dolphinAvg);
 //avg of Koala's games
 let koalaAvg = (kGame1 + kGame2 + kGame3) / numGames;
+koalaAvg = Math.round(koalaAvg);
+
+console.log(`Dolphins average score is ${dolphinAvg}`);
+console.log(`Koalas average score is ${koalaAvg}`);
+
+if (dolphinAvg > koalaAvg) {
+  if (dolphinAvg > 100) {
+    console.log(`Dolphins win with an average score of ${dolphinAvg}`);
+  } else {
+    console.log(
+      `Dolphins had a higher score but didn't meet the minimum of 100 points!`
+    );
+  }
+} else if (koalaAvg > 100) {
+  console.log(`Koalas win with an average score of ${koalaAvg}`);
+}
+
+// console.log(`Dolphins win with an average score of ${dolphinAvg}`);
+// console.log(`Koalas win with an average score of ${koalaAvg}`);

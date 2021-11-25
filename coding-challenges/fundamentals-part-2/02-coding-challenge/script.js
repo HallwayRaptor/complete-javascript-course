a7ae91beaa97f4c5744d7ff9b6f49d81c2e11ca5
Rob Whelan <hallwayraptor@gmail.com>
2021-11-25 17:01:30 -0400

    finish fundamentals 2

--- coding-challenges/fundamentals-part-2/02-coding-challenge/script.js
+++ coding-challenges/fundamentals-part-2/02-coding-challenge/script.js
@@ -11,21 +11,35 @@ const calcTip = function (bill) {
 	}
 };
 
+const calcAverage = function (arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum = sum + arr[i];
+	}
+	let avg = sum / arr.length;
+	console.log((Math.round(avg) * 10) / 10);
+};
+
 // bill amount
 //let bill = 100;
 
-const bills = [125, 555, 44];
-const tips = ["", "", ""];
-const total = ["", "", ""];
-
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const total = [];
 // tip 15% or 20%
-
 tips[0] = calcTip(bills[0]);
 tips[1] = calcTip(bills[1]);
 tips[2] = calcTip(bills[2]);
-console.log(`The tips are: ${tips}`);
-
+// console.log(`The tips are: ${tips}`);
 total[0] = tips[0] + bills[0];
 total[1] = tips[1] + bills[1];
 total[2] = tips[2] + bills[2];
-console.log(`The totals are: ${total}`);
+// console.log(`The totals are: ${total}`);
+
+for (let i = 0; i < bills.length; i++) {
+	//tips.push(calcTip(bills[i]));
+	total.push(calcTip(bills[i]) + bills[i]);
+}
+console.log(`TOTALS: ${total}`);
+
+calcAverage(total);
